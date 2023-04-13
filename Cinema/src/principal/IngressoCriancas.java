c9c5c3e1bc9a240b4d94b36c07943c02a0429c68
mateu <mateu@Candiollis>
2023-04-13 07:49:52 -0300

    Primeiro commit desafio cinema

---
+++ Cinema/src/principal/IngressoCriancas.java
@@ -0,0 +1,19 @@
+package principal;
+
+public class IngressoCriancas {
+	
+	private final char tipo = 'C';
+	private final double ingresso = 5.5;
+	
+	public IngressoCriancas() {
+		
+	}
+	
+	public char getTipo() {
+		return tipo;
+	}
+
+	public double getIngreco() {
+		return ingresso;
+	}
+}
---
+++ Cinema/src/principal/IngressoEstudantes.java
@@ -0,0 +1,19 @@
+package principal;
+
+public class IngressoEstudantes {
+	
+	private final char tipo = 'E';
+	private final double ingresso = 8;
+	
+	public IngressoEstudantes() {
+		
+	}
+	
+	public char getTipo() {
+		return tipo;
+	}
+
+	public double getIngresso() {
+		return ingresso;
+	}
+}
