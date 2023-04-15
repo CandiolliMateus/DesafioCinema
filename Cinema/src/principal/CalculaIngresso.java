8834048a739136db5c697db84f64fc1468e6d865
mateu <mateu@192.168.2.102>
2023-04-15 00:47:52 -0300

    Commit adicionando testes

--- Cinema/src/principal/CalculaIngresso.java
+++ Cinema/src/principal/CalculaIngresso.java
@@ -3,9 +3,231 @@ package principal;
 public class CalculaIngresso {
 	
 	double ingressoComDesconto;
+	double ingressoAPagar;
 	
 	Ingresso ingresso = new Ingresso();
 	
+	public double calculoIngresso(boolean carteiraEstudante, int diaDaSemana, char tipoDeIngresso) {
+		
+		if(carteiraEstudante == ingresso.ingressoEstudantes.isCarteiraEstudante()) {
+			
+			if(diaDaSemana == ingresso.diaDaSemana.getOutros()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoAPagar = ingresso.ingressoCriancas.getIngreco();
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoAPagar = ingresso.ingressoEstudantes.getIngresso();
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.05);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar =  ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getSegunda()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+				
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.1);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.35);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.1);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getTerca()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.15);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.35);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.15);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getQuarta()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.3);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.35);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.4);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getQuinta()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoAPagar = ingresso.ingressoCriancas.getIngreco();
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.35);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.3);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getSexta()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.11);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.35);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoAPagar = ingresso.ingressoIdosos.getIngresso();
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			}
+		} else {
+			
+			if(diaDaSemana == ingresso.diaDaSemana.getOutros()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoAPagar = ingresso.ingressoCriancas.getIngreco();
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoAPagar = ingresso.ingressoEstudantes.getIngresso();
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.05);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getSegunda()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.1);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.1);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.1);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getTerca()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.15);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.05);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.15);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getQuarta()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.3);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.5);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.4);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getQuinta()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+				
+					ingressoAPagar = ingresso.ingressoCriancas.getIngreco();
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoEstudantes.getIngresso() - (ingresso.ingressoEstudantes.getIngresso() * 0.3);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoComDesconto = ingresso.ingressoIdosos.getIngresso() - (ingresso.ingressoIdosos.getIngresso() * 0.3);
+					ingressoAPagar = ingressoComDesconto;
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			} else if(diaDaSemana == ingresso.diaDaSemana.getSexta()) {
+				
+				if (tipoDeIngresso == ingresso.ingressoCriancas.getTipo()) {
+					
+					ingressoComDesconto = ingresso.ingressoCriancas.getIngreco() - (ingresso.ingressoCriancas.getIngreco() * 0.11);
+					ingressoAPagar = ingressoComDesconto;
+				} else if (tipoDeIngresso == ingresso.ingressoEstudantes.getTipo()) {
+					
+					ingressoAPagar = ingresso.ingressoEstudantes.getIngresso();
+				} else if (tipoDeIngresso == ingresso.ingressoIdosos.getTipo()) {
+
+					ingressoAPagar = ingresso.ingressoIdosos.getIngresso();
+				} else {
+					
+					ingressoAPagar = ingresso.getingressoInteiro();
+				}
+			}
+		}
+		return ingressoAPagar;
+	}
+
+	
+	/**
 	public void calculoIngresso(boolean carteiraEstudante, int diaDaSemana, char tipoDeIngresso) {
 		
 		if(carteiraEstudante == ingresso.ingressoEstudantes.isCarteiraEstudante()) {
@@ -238,5 +460,6 @@ public class CalculaIngresso {
 				System.out.println("Sextta feira.");
 			}
 		}
-	}	
+	}
+	**/	
 }
